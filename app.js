ac10c027c62cdcaf2c79d10d49489e4e28c5f5f3
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2022-01-20 13:16:13 +0530

    final appp

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var add_Button = document.getElementById('addButton');
 var task = document.getElementById('input_data');
 var list = document.getElementById('taskList');
 
+//  implement addEventListener to the  form from  where we get data
 task.addEventListener('submit' ,(event)=>{
+    // get the value of text
     var input_text = document.getElementById('text').value;
     event.preventDefault();
+    // creating template to add dynamic list
     let template =`<div class ="addTask" onClick ="this.remove()">
     <div class ="task"><li>${input_text} </div>
-    <div class ="deleteTask"><button class ="deleteButton" >X</button></div></li>
+    <div class ="deleteTask"><button class ="deleteButton"  >X</button></div></li>
     </div>`;
+    // creating element
     const divElement =document.createElement('div');
     divElement.innerHTML = template;
+    // adding divElement as child of div
     list.appendChild(divElement);
+    // change the value of input to blank
     document.getElementById('text').value ="";
 }
 
 );
+
+
+
