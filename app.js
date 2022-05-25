caccc98424432a003e33d9d571f5bd3e6fd939c0
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2022-05-25 16:06:49 +0530

    change in design

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
 var task = document.getElementById('input_data');
 var list = document.getElementById('taskList');
 var task_count = document.getElementById('taskCount');
-var count  = 0 ;
-
+var count = 0;
+var arrayOfTask = [];
 
 //  implement addEventListener to the  form from  where we get data
-task.addEventListener('submit' ,(event)=>{
+task.addEventListener('submit', (event) => {
     // get the value of text
     var input_text = document.getElementById('text').value;
+    document.getElementById('error').innerHTML = "";
     event.preventDefault();
-    
-    // creating template to add dynamic list
-    let template =`<div class ="addTask">
+    var index = arrayOfTask.indexOf(input_text);
+    if (index == -1) {
+        arrayOfTask.push(input_text);
+        // creating template to add dynamic list
+        let template = `<div class ="addTask">
+        
     ${input_text}<b><button class ="deleteButton" onmouseover ="myremove()" >X</button><b></div>`;
-    // creating element
-    const divElement =document.createElement('li');
-    divElement.innerHTML = template;
-    // adding divElement as child of div
-    list.appendChild(divElement);
-    count++;
-    task_count.innerHTML =count +' tasks Left';
+        // creating element
+        const divElement = document.createElement('li');
+        divElement.innerHTML = template;
+        // adding divElement as child of div
+        list.appendChild(divElement);
+        count++;
+        task_count.innerHTML = count + ' tasks Left';
+    } else {
+        document.getElementById('error').textContent = "Task Already Added !!";
+        document.getElementById('error').style.color = "red";
+        return false;
+    }
     // change the value of input to blank
-    document.getElementById('text').value ="";
+    document.getElementById('text').value = "";
 }
 
 );
 
- function myremove(){
+function myremove() {
     var getAllTaskList = document.querySelectorAll('li');
-     for(var i = 0 ; i< getAllTaskList.length ; i++){
-        var data = getAllTaskList[i].innerHTML; 
-        getAllTaskList[i].onclick = function(){
-             this.remove();
-             count--;
-             task_count.innerHTML =count +' tasks Left';
+    for (var i = 0; i < getAllTaskList.length; i++) {
+        var data = getAllTaskList[i].innerHTML;
+        getAllTaskList[i].onclick = function () {
+            this.remove();
+            count--;
+            task_count.innerHTML = count + ' tasks Left';
         }
-        
+
     }
-    
-     
- }
+
+
+}
 
 
