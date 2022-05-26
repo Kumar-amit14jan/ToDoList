26cb2434ac0d51250cc44d12d6f70396931117a6
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2022-05-27 01:57:56 +0530

    Some change in style

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 var task = document.getElementById('input_data');
-var list = document.getElementById('taskList');
+var list = document.getElementById('task');
+var taskAdd = document.getElementById('addTask');
 var task_count = document.getElementById('taskCount');
 var count = 0;
 var arrayOfTask = [];
+var today = new Date();
+console.log(date);
+var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var day = today.getDay();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+document.getElementById('dateWithDays').innerHTML = weekDays[day] + '  ' + time;
+
+// if(arrayOfTask.length ==0){
+//     list.innerHTML+=`<tr id="emptyData"> Your List Is Empty !!</tr>`;
+// }
 
 //  implement addEventListener to the  form from  where we get data
-task.addEventListener('submit', (event) => {
+taskAdd.addEventListener('click', (event) => {
     // get the value of text
     var input_text = document.getElementById('text').value;
+    if (input_text.length == 0) {
+        document.getElementById('error').textContent = "Please Add Task !!";
+        document.getElementById('error').style.color = "red";
+        return false;
+    }
+    var date = document.getElementById('date').value;
     document.getElementById('error').innerHTML = "";
-    event.preventDefault();
     var index = arrayOfTask.indexOf(input_text);
     if (index == -1) {
         arrayOfTask.push(input_text);
         // creating template to add dynamic list
-        let template = `<div class ="addTask">
-        
-    ${input_text}<b><button class ="deleteButton" onmouseover ="myremove()" >X</button><b></div>`;
-        // creating element
-        const divElement = document.createElement('li');
-        divElement.innerHTML = template;
-        // adding divElement as child of div
-        list.appendChild(divElement);
+        let length = arrayOfTask.length;
+        let template = `<tr >  
+        <td>${length}</td><td>${date}</td><td>${input_text}</td><td><span class ="deleteButton" onmouseover ="myremove()" ></i><i class="fa-solid fa-trash-can"></i>&nbspDelete</span></td></tr>`;
+        list.innerHTML += template;
         count++;
         task_count.innerHTML = count + ' tasks Left';
     } else {
@@ -31,14 +43,17 @@ task.addEventListener('submit', (event) => {
     }
     // change the value of input to blank
     document.getElementById('text').value = "";
+    document.getElementById('date').value = "";
 }
 
 );
 
 function myremove() {
-    var getAllTaskList = document.querySelectorAll('li');
+    var getAllTaskList = document.querySelectorAll('tr');
     for (var i = 0; i < getAllTaskList.length; i++) {
         var data = getAllTaskList[i].innerHTML;
+        var removeTaskIndex = arrayOfTask.indexOf(getAllTaskList[i].children[i].textContent);
+        arrayOfTask.splice(removeTaskIndex, 1);
         getAllTaskList[i].onclick = function () {
             this.remove();
             count--;
